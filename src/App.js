e13e040cd9fccb7087d1fd52b21dfaf25455b1ee
jeongnaehyeok <cnh0301@gmail.com>
2020-12-25 02:33:23 +0900

    [ Feature ] Page 나누기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+
+import Navbar from '@pages/Navbar';
+import Home from '@pages/Home';
+import Menu from '@pages/Menu';
+import Basket from '@pages/Basket';
+
 import '@scss/App.scss';
 
 const App = () => {
   return (
-    <div className="app">
-      <h1>Test</h1>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <main>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/menu/:kind" exact component={Menu} />
+          <Route path="/basket" exact component={Basket} />
+          <Redirect path="*" to="/" />
+        </Switch>
+      </main>
+    </BrowserRouter>
   );
 };
 
